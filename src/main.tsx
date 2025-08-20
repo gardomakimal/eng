fe591e9804aaf95f73823299fa0d5b5da0ba3ef0
[dyad] <git@dyad.sh>
2025-08-20 02:14:56 +0100

    [dyad] Adding i18n for German and French - wrote 8 file(s), added i18next, react-i18next, i18next-browser-languagedetector, i18next-http-backend package(s) + extra files edited outside of Dyad

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import "./i18n";
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider attribute="class" defaultTheme="dark" forcedTheme="dark">
