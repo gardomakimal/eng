b10643f5d21e9024974c61776557480cb2396fb2
[dyad] <gardomakimal@gmail.com>
2025-09-13 15:34:08 +0100

    [dyad] Added unclickable menu icon to header - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Menu } from "lucide-react"; 
+import { Menu } from "lucide-react";
 
 const Header: React.FC = () => {
   return (
@@ -8,15 +8,15 @@ const Header: React.FC = () => {
       <div className="flex items-center space-x-2 w-1/3">
         <img src="/logo.png" alt="Logo" className="h-8" />
       </div>
-      
+
       {/* Middle section: Empty or can be removed if not needed */}
       <div className="flex justify-center flex-grow">
         {/* This section is now empty */}
       </div>
-      
-      {/* Right section: Empty, as LanguageSwitcher is removed */}
-      <div className="flex items-center justify-end space-x-4 w-1/3">
-        {/* LanguageSwitcher was here */}
+
+      {/* Right section: Menu icon */}
+      <div className="flex items-center justify-end w-1/3">
+        <Menu className="h-6 w-6 text-gray-600 cursor-default" />
       </div>
     </header>
   );
